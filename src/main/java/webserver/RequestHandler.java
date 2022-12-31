0668ee50d9a31873c3749fce17d198e26a02aa0a
김현교 <zkdlajt96@naver.com>
2023-01-01 00:12:01 +0900

    feat(chapter3) : 요구사항4, 302 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,17 +1,16 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Map;
-
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 import util.StartLine;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -28,30 +27,27 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            //1. Start Line 파싱 -> Method, url, version
-            String firstLine = br.readLine();
-            StartLine startLine = HttpRequestUtils.parseStartLine(firstLine);
-            //2. Header 파싱 -> Map으로 리턴
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            StartLine startLine = HttpRequestUtils.parseStartLine(br.readLine());
             HttpHeader header = HttpRequestUtils.parseHeaders(br);
-            //3. Data 파싱 -> readData 활용
             String data = null;
             if ("POST".equals(startLine.getMethod())) {
                 int contentLength = Integer.parseInt(header.get(HttpHeader.CONTENT_LENGTH));
                 data = IOUtils.readData(br, contentLength);
                 log.debug("HTTP Request Data = {}", data);
             }
-            //4. 3개의 데이터 조합해서 할일 처리
-            //  - url이 webapp폴더에 있는 파일일 경우 -> 파일 읽기
-            //  - /create 등 원하는 기능인 경우 -> 해당 기능 실행, 응답 데이터 만들기
+
             byte[] body = "Hello World".getBytes();
             if ("/user/create".equals(startLine.getUrl())) {
-                //회원가입 처리하기
                 Map<String, String> dataMap = HttpRequestUtils.parseData(data);
                 User user = new User(dataMap.get("userId"), dataMap.get("password"), dataMap.get("name"), dataMap.get("email"));
                 log.debug("Join user = {}", user);
+
+                response302Header(dos, header.get("Host"));
+                return;
             } else {
-                //body = file에서 읽어오기
                 String url = startLine.getUrl();
                 if ("/".equals(url)) {
                     url = DefaultURL;
@@ -59,11 +55,19 @@ public class RequestHandler extends Thread {
                 String path = String.format("./webapp%s", url);
                 body = Files.readAllBytes(new File(path).toPath());
             }
-            //5. 응답 데이터 출력
-            //Output 만들기
-            DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String host) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes(String.format("Location: http://%s%s\r\n", host, DefaultURL));
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
