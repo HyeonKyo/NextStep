9290f4766b6458ef503a67fb3bf3927610d02558
김현교 <zkdlajt96@naver.com>
2023-01-04 22:17:47 +0900

    refactor(chapter5) : 요청 분기문 메소드 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -29,43 +29,59 @@ public class RequestHandler extends Thread {
             HttpResponse response = new HttpResponse(out);
 
             if ("/user/create".equals(request.getPath())) {
-                User user = new User(request.getParameter("userId"),
-                        request.getParameter("password"),
-                        request.getParameter("name"),
-                        request.getParameter("email"));
-                log.debug("Join user = {}", user);
-                DataBase.addUser(user);
-
-                response.sendRedirect(DEFAULT_URL);
+                createUser(request, response);
             } else if ("/user/login".equals(request.getPath())) {
-                String uri = DEFAULT_URL;
-                boolean isSuccess = true;
-                User user = DataBase.findUserById(request.getParameter("userId"));
-                if (user == null) {
-                    isSuccess = false;
-                    uri = "/user/login_failed.html";
-                }
-                response.setCookie("logined", String.valueOf(isSuccess));
-                response.sendRedirect(uri);
+                loginUser(request, response);
             } else if ("/user/list".equals(request.getPath())) {
-                String uri = "/user/login";
-                boolean logined = Boolean.parseBoolean(request.getCookie("logined"));
-                log.debug("Is Login = {}", logined);
-                if (!logined) {
-                    response.sendRedirect(uri);
-                } else {
-                    String path = "/user/list.html";
-                    response.forward(path);
-                }
+                listUser(request, response);
             } else {
-                String url = request.getPath();
-                if ("/".equals(url)) {
-                    url = DEFAULT_URL;
-                }
-                response.forward(url);
+                defaultForward(request, response);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void defaultForward(HttpRequest request, HttpResponse response) throws IOException {
+        String url = request.getPath();
+        if ("/".equals(url)) {
+            url = DEFAULT_URL;
+        }
+        response.forward(url);
+    }
+
+    private void listUser(HttpRequest request, HttpResponse response) throws IOException {
+        String uri = "/user/login";
+        boolean logined = Boolean.parseBoolean(request.getCookie("logined"));
+        log.debug("Is Login = {}", logined);
+        if (!logined) {
+            response.sendRedirect(uri);
+        } else {
+            String path = "/user/list.html";
+            response.forward(path);
+        }
+    }
+
+    private void loginUser(HttpRequest request, HttpResponse response) {
+        String uri = DEFAULT_URL;
+        boolean isSuccess = true;
+        User user = DataBase.findUserById(request.getParameter("userId"));
+        if (user == null) {
+            isSuccess = false;
+            uri = "/user/login_failed.html";
+        }
+        response.setCookie("logined", String.valueOf(isSuccess));
+        response.sendRedirect(uri);
+    }
+
+    private void createUser(HttpRequest request, HttpResponse response) {
+        User user = new User(request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+        log.debug("Join user = {}", user);
+        DataBase.addUser(user);
+
+        response.sendRedirect(DEFAULT_URL);
     }
 }
